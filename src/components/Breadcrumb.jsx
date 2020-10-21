148fbc3aa2f21d9b049526645131a36568dbfb33
Michael Wanyoike <micwanyoike@gmail.com>
2020-10-21 11:52:58 +0300

    Fixed jsx issues with Breadcrumb

--- src/components/Breadcrumb.jsx
+++ src/components/Breadcrumb.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const Breadcrumb = ({ page }) => (
-  <div class="px-4 py-6 text-sm text-gray-500 bg-gray-800">
-    <div class="container mx-auto">
-      <ul class="flex items-end space-x-2 font-semibold">
+  <div className="px-4 py-6 text-sm text-gray-500 bg-gray-800">
+    <div className="container mx-auto">
+      <ul className="flex items-end space-x-2 font-semibold">
         <li>
           <Link
-            class="text-gray-300 hover:underline hover:text-gray-100"
+            className="text-gray-300 hover:underline hover:text-gray-100"
             to="/"
           >
             Home
@@ -15,16 +15,16 @@ const Breadcrumb = ({ page }) => (
         </li>
         <li>
           <svg
-            class="w-4 h-4"
+            className="w-4 h-4"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="3"
               d="M9 5l7 7-7 7"
             ></path>
           </svg>
